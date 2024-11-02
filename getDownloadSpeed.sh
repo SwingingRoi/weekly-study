e685a064f670e77c912e8da41c036bcd602dee66
Swinging Roi <32241007+SwingingRoi@users.noreply.github.com>
2024-11-02 12:55:56 +0800

    Update getDownloadSpeed.sh
    修改代码实现与等待时间 提高兼容性与性能

--- getDownloadSpeed.sh
+++ getDownloadSpeed.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # 监控的网络接口名称，例如 eth0, wlan0 等
-INTERFACE="eth1"
+INTERFACE=$1
 
 # 获取初始接收字节数
 initial_bytes=$(cat /proc/net/dev | grep "$INTERFACE" | tr -s ' ' | cut -d ' ' -f 3)
 
 # 等待一秒
-sleep 1
+sleep  0.1
 
 # 获取下一次接收字节数
 final_bytes=$(cat /proc/net/dev | grep "$INTERFACE" | tr -s ' ' | cut -d ' ' -f 3)
 
 # 计算下载的字节数
-bytes_downloaded=$((final_bytes - initial_bytes))
+bytes_downloaded=$((final_bytes - initial_bytes)*1/sleep)
 
 # 转换为 MB/s
 mb_per_sec=$(echo "scale=2; $bytes_downloaded / 1024 / 1024" | bc)
